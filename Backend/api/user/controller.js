83ace12fa177d299fb67bef36837048e3d4fbc1f
FirescuOvidiu <ovidiufirescu@gmail.com>
2021-01-29 01:30:54 +0200

    Added route and controller method for register

--- Backend/api/user/controller.js
+++ Backend/api/user/controller.js
@@ -1,7 +1,12 @@
+const register = async (req, res) => {
+  res.status(200).send("Register Succesful");
+};
+
 const login = async (req, res) => {
   res.status(200).send("Login Succesful");
 };
 
 module.exports = {
+  register,
   login,
 };
--- Backend/api/user/router.js
+++ Backend/api/user/router.js
@@ -2,6 +2,9 @@ const userController = require("./controller");
 
 const router = require("express").Router();
 
+// Route to register a new user with email and password
+router.post("/register", userController.register);
+
 // Route used to login with email and password
 router.post("/login", userController.login);
 
