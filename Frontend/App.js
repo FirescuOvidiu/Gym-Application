51a2ca41935b85b400ea215fbadc06ffad6e63fc
FirescuOvidiu <ovidiufirescu@gmail.com>
2021-02-06 00:36:37 +0200

    Added Provider

--- Frontend/App.js
+++ Frontend/App.js
@@ -1,14 +1,18 @@
 import * as React from 'react';
+import {Provider} from 'react-redux';
+import {createStore} from 'redux';
+import 'react-native-gesture-handler';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import 'react-native-gesture-handler';
 
 import LoginScreen from './src/screens/loginScreen';
 import RegisterScreen from './src/screens/registerScreen';
 import TabNavigatorRoutes from './src/screens/tabNavigatorRoutes';
+import userReducer from './src/redux/reducers/userReducer';
 
 const Stack = createStackNavigator();
+const store = createStore(userReducer);
 
 const Auth = () => {
   return (
@@ -29,20 +33,22 @@ const Auth = () => {
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Auth">
-        <Stack.Screen
-          name="Auth"
-          component={Auth}
-          options={{headerShown: false}}
-        />
-        <Stack.Screen
-          name="TabNavigatorRoutes"
-          component={TabNavigatorRoutes}
-          options={{headerShown: false}}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Auth">
+          <Stack.Screen
+            name="Auth"
+            component={Auth}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="TabNavigatorRoutes"
+            component={TabNavigatorRoutes}
+            options={{headerShown: false}}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 };
 
