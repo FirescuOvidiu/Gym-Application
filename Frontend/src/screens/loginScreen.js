6b3d8e25f414b6bd59642704054e81c54dfcdc97
FirescuOvidiu <ovidiufirescu@gmail.com>
2021-02-05 11:51:42 +0200

    Implemented login screen

--- Frontend/src/screens/loginScreen.js
+++ Frontend/src/screens/loginScreen.js
@@ -1,15 +1,72 @@
-import * as React from 'react';
-import {Text, View} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, View, Text, TouchableOpacity} from 'react-native';
 
-const LoginScreen = () => {
+import AsyncStorage from '@react-native-community/async-storage';
+
+const axios = require('axios').default;
+
+const LoginScreen = ({navigation}) => {
+  const [userEmail, setUserEmail] = useState('');
+  const [userPassword, setUserPassword] = useState('');
+
+  const handleSubmitPress = async () => {
+    if (!userEmail) {
+      alert('Please fill Email');
+      return;
+    }
+    if (!userPassword) {
+      alert('Please fill Password');
+      return;
+    }
+
+    try {
+      const response = await axios.post(
+        `http://192.168.100.2:3000/api/user/login`,
+        {
+          email: userEmail,
+          password: userPassword,
+        },
+      );
+
+      await AsyncStorage.setItem('accessToken', response.data.accessToken);
+      //navigation.replace('');
+    } catch (error) {
+      alert(error.response.data.message);
+    }
+  };
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-      <Text>Hello, world!</Text>
+    <View>
+      <Text>Welcome back</Text>
+      <View>
+        <Text>Email</Text>
+        <TextInput
+          onChangeText={(UserEmail) => setUserEmail(UserEmail)}
+          placeholder="Enter Email"
+          returnKeyType="next"
+          blurOnSubmit={false}
+        />
+      </View>
+      <View>
+        <Text>Password</Text>
+        <TextInput
+          onChangeText={(UserPassword) => setUserPassword(UserPassword)}
+          placeholder="Enter Password"
+          blurOnSubmit={false}
+          returnKeyType="next"
+          secureTextEntry={true}
+        />
+      </View>
+      <Text onPress={() => navigation.navigate('')}>Forgot password?</Text>
+      <TouchableOpacity onPress={() => handleSubmitPress()}>
+        <Text>Sign in</Text>
+      </TouchableOpacity>
+      <View style={{flexDirection: 'row'}}>
+        <Text> Don't have an account? </Text>
+        <Text onPress={() => navigation.navigate('RegisterScreen')}>
+          Sign Up
+        </Text>
+      </View>
     </View>
   );
 };
