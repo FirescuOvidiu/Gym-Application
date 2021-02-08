5a56738724a0be134dc84aedca70d6262e4c7640
FirescuOvidiu <ovidiufirescu@gmail.com>
2021-02-08 06:13:57 +0200

    Implemented update gym method

--- Backend/api/gym/controller.js
+++ Backend/api/gym/controller.js
@@ -13,7 +13,29 @@ const createGym = async (req, res, next) => {
   }
 };
 
-const updateGym = async (req, res, next) => {};
+const updateGym = async (req, res, next) => {
+  try {
+    let gym = await Gym.findById(req.params._id);
+
+    if (!gym) {
+      return next({ message: "The gym was not found." });
+    }
+
+    gym.name = req.body.name || gym.name;
+    gym.email = req.body.email || gym.email;
+    gym.phone = req.body.phone || gym.phone;
+    gym.address = req.body.address || gym.address;
+    gym.birthday = req.body.birthday || gym.birthday;
+    gym.openingTime = req.body.openingTime || gym.openingTime;
+    gym.closingTime = req.body.closingTime || gym.closingTime;
+
+    gym = await gym.save();
+
+    res.status(200).json({ status: "The gym was updated." });
+  } catch (error) {
+    return next(error);
+  }
+};
 
 const deleteGym = async (req, res, next) => {
   try {
