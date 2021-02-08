dc579b707b14f4acfae4088b5884337bf3c4a2a9
FirescuOvidiu <ovidiufirescu@gmail.com>
2021-02-08 03:34:21 +0200

    Modified Splash Screen

--- Frontend/src/screens/splashScreen.js
+++ Frontend/src/screens/splashScreen.js
@@ -1,14 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Text} from 'react-native';
 
 import AsyncStorage from '@react-native-community/async-storage';
 
 const SplashScreen = ({navigation}) => {
+  setTimeout(async () => {
     const token = await AsyncStorage.getItem('accessToken');
     navigation.replace(token === null ? 'Auth' : 'TabNavigatorRoutes');
+  }, 1000);
 
   return (
     <View>
+      <Text>SplashScreen</Text>
     </View>
   );
 };
