74a933937d65e67fd4cc8da8737f13344d882924
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-03-29 07:15:27 +0300

    Implemented Workout Details Screen

--- Frontend/src/screens/workoutDetailsScreen.js
+++ Frontend/src/screens/workoutDetailsScreen.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList} from 'react-native';
 
-const WorkoutDetailsScreen = () => {
+const WorkoutDetailsScreen = ({route, navigation}) => {
+  let workout = route.params.workout;
+
   return (
     <View>
-      <Text>Test2</Text>
+      <Text>name: {workout.name}</Text>
+      <Text>date: {workout.date}</Text>
+      <Text>type: {workout.type}</Text>
+      <FlatList
+        data={workout.exercises}
+        keyExtractor={(item) => item.name}
+        renderItem={({item}) => {
+          return (
+            <View>
+              <Text>Exercise name: {item.name}</Text>
+              <Text>Exercise reps: {item.reps}</Text>
+              <Text>Exercise sets: {item.sets}</Text>
+              <Text>Exercise weight: {item.weight}</Text>
+            </View>
+          );
+        }}
+      />
+      <Text>notes: {workout.notes}</Text>
     </View>
   );
 };
