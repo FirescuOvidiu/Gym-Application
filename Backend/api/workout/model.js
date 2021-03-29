3d6a3c990e4665a3788a742fcb40198dcb461d55
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-03-29 13:31:51 +0300

    Minor change

--- Backend/api/workout/model.js
+++ Backend/api/workout/model.js
@@ -37,14 +37,14 @@ const workoutSchema = new mongoose.Schema({
         },
         weight: {
           type: Number,
-          default: "",
+          default: 0,
         },
       },
     ],
   },
   notes: {
     type: String,
-    default: 0,
+    default: "",
   },
 });
 
