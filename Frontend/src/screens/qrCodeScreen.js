69ceea811c477120c8605e635d15b4ef4d6bfe94
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-04-07 11:02:57 +0300

    Added additional packages and improved qrCodeScreen

--- Frontend/src/screens/qrCodeScreen.js
+++ Frontend/src/screens/qrCodeScreen.js
@@ -1,10 +1,39 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity, SafeAreaView} from 'react-native';
 
+import QRCodeScanner from 'react-native-qrcode-scanner';
 const QRCodeScreen = () => {
+  const [scan, setScan] = useState(false);
+  const [result, setResult] = useState();
+
   return (
     <View>
-      <Text>SplashScreen</Text>
+      {!scan && (
+        <View>
+          <TouchableOpacity
+            onPress={() => {
+              setScan(true);
+              setResult();
+            }}>
+            <Text>Start Scan</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      {scan && (
+        <QRCodeScanner
+          reactivate={true}
+          showMarker={true}
+          topContent={<Text>Scan your QRCode!</Text>}
+          onRead={(e) => {
+            console.log(e.data);
+          }}
+          bottomContent={
+            <TouchableOpacity onPress={() => setScan(false)}>
+              <Text>Cancel Scan</Text>
+            </TouchableOpacity>
+          }
+        />
+      )}
     </View>
   );
 };
