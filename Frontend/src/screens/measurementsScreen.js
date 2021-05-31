0336c04bb8a4149a4415983a36f7c0936a76df63
Ovidiu Firescu <ovidiufirescu@gmail.com>
2021-05-31 11:31:08 +0300

    Updated and improved style for Measurements Screen

--- Frontend/src/screens/measurementsScreen.js
+++ Frontend/src/screens/measurementsScreen.js
@@ -1,15 +1,21 @@
 import React, {useState} from 'react';
 
-import {View, StyleSheet, Alert} from 'react-native';
+import {View, StyleSheet, Alert, Modal, Text} from 'react-native';
 
 import AuthInputField from '../components/authInputField';
 import SignButton from '../components/signButton';
 
 const MeasurementsScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   const [userHeight, setUserHeight] = useState(0);
   const [userWeight, setUserWeight] = useState(0);
   const [userAge, setUserAge] = useState(0);
   const [userGender, setUserGender] = useState('');
+  const [bmi, setBmi] = useState(0);
+  const [category, setCategory] = useState('');
+  const [lowerNormalWeight, setLowerNormalWeight] = useState(0);
+  const [upperNormalWeight, setUpperNormalWeight] = useState(0);
+  const [bmr, setBmr] = useState(0);
 
   const handleSubmitButton = async () => {
     if (!userHeight) {
@@ -33,40 +39,24 @@ const MeasurementsScreen = () => {
       return;
     }
 
-    const bmi = ((userWeight / (userHeight * userHeight)) * 10000).toFixed(1),
-      category =
-        bmi < 18.5
-          ? 'Underweight'
-          : bmi < 25
-          ? 'Normal'
-          : bmi < 30
-          ? 'Overweight'
-          : 'Obese',
-      lowerNormalWeight = (0.00185 * userHeight * userHeight).toFixed(1),
-      upperNormalWeight = (0.00249 * userHeight * userHeight).toFixed(1);
+    setModalVisible(true);
 
-    let bmr = 10 * userWeight + 6.25 * userHeight - 5 * userAge;
-    if (userGender === 'male') {
-      bmr = bmr + 5;
-    } else {
-      bmr = bmr - 161;
-    }
-
-    Alert.alert(
-      'Results',
-      `Your BMI is ${bmi}, indicating you're in the ${category} category for adults of your height. For your height, a normal weight range would be from ${lowerNormalWeight} to ${upperNormalWeight}. Your BMR is ${bmr}.
-      Based on your activity factor you should consume ${
-        bmr * 1.2
-      } calories if you do little to no exercise,
-      ${bmr * 1.375} calories if you do light exercise/sports 1-3 days/week, ${
-        bmr * 1.55
-      } calories
-      if you do moderate exercise/sports 3-5 days/week, ${
-        bmr * 1.725
-      } calories if you 
-      do hard exercise/sports 6-7 days a week, ${
-        bmr * 1.9
-      } very hard exercise/sports & a physical job`,
+    setBmi(((userWeight / (userHeight * userHeight)) * 10000).toFixed(1));
+    setCategory(
+      bmi < 18.5
+        ? 'Underweight'
+        : bmi < 25
+        ? 'Normal'
+        : bmi < 30
+        ? 'Overweight'
+        : 'Obese',
+    );
+    setLowerNormalWeight((0.00185 * userHeight * userHeight).toFixed(1));
+    setUpperNormalWeight((0.00249 * userHeight * userHeight).toFixed(1));
+    setBmr(
+      userGender === 'male'
+        ? 10 * userWeight + 6.25 * userHeight - 5 * userAge + 5
+        : 10 * userWeight + 6.25 * userHeight - 5 * userAge - 161,
     );
   };
 
@@ -78,6 +68,44 @@ const MeasurementsScreen = () => {
           <AuthInputField title="Weight" setData={setUserWeight} />
           <AuthInputField title="Age" setData={setUserAge} />
           <AuthInputField title="Gender" setData={setUserGender} />
+          <Modal animationType="slide" visible={modalVisible}>
+            <View style={styles.modalView}>
+              <Text>
+                {'    '}Your BMI is {bmi}, indicating you're in the category for
+                adults of your height. For your height, a normal weight range
+                would be from {lowerNormalWeight} to {upperNormalWeight}.
+                {'\n\n'}
+                {'    '}Your BMR is {bmr}. Based on your activity factor you
+                should consume:
+              </Text>
+              <Text style={styles.bmrText}>
+                - {(bmr * 1.2).toFixed(0)} calories if you do little to no
+                exercise
+              </Text>
+              <Text style={styles.bmrText}>
+                - {(bmr * 1.375).toFixed(0)} calories if you do light
+                exercise/sports 1-3 days/week
+              </Text>
+              <Text style={styles.bmrText}>
+                - {(bmr * 1.55).toFixed(0)} calories if you do moderate
+                exercise/sports 3-5 days/week
+              </Text>
+              <Text style={styles.bmrText}>
+                - {(bmr * 1.725).toFixed(0)} calories if you do hard
+                exercise/sports 6-7 days a week
+              </Text>
+              <Text style={styles.bmrText}>
+                - {(bmr * 1.9).toFixed(0)} very hard exercise/sports & a
+                physical job
+              </Text>
+              <SignButton
+                submit={() => {
+                  setModalVisible(false);
+                }}
+                text="Cancel"
+              />
+            </View>
+          </Modal>
           <SignButton submit={handleSubmitButton} text="Calculate" />
         </View>
       </View>
@@ -96,6 +124,16 @@ const styles = StyleSheet.create({
   insideBody: {
     flex: 1,
     margin: '5%',
+  },
+  bmrText: {
+    marginBottom: '2%',
+  },
+  modalView: {
+    margin: 20,
+    padding: '5%',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
   },
 });
 
